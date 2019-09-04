3c0d6622ce86aaecf84adbd24b8cf52de4972e46
Ulas Tuerkmen <ulas.tuerkmen@gmail.com>
2019-09-04 22:12:13 +0200

    Tag more resources, better cleanup

--- checkpoints-part-2.sh
+++ checkpoints-part-2.sh
@@ -16,7 +16,8 @@ aws ecs create-cluster --cluster-name demo-cluster --tags key=Environment,value=
 
 ROLEARN=$(aws iam create-role --role-name ecsTaskExecutionRole \
   --assume-role-policy-document "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"Service\":[\"ecs-tasks.amazonaws.com\"]},\"Action\":[\"sts:AssumeRole\"]}]}" \
-  --query "Role.Arn" --output text)
+  --query "Role.Arn" --output text
+  --tags Key=Environment,Value=Demo)
 
 POLICYARN=$(aws iam list-policies --query 'Policies[?PolicyName==`AmazonECSTaskExecutionRolePolicy`].{ARN:Arn}' \
   --output text)
@@ -98,14 +99,20 @@ aws ecr delete-repository --repository-name simple-app --force
 
 LBARN=$(aws elbv2 create-load-balancer --tags Key=Environment,Value=Demo --name demo-balancer \
   --type application --subnets $SUBNETID $SUBNET2ID --security-groups $SECURITYGROUPID \
+  --tags Key=Environment,Value=Demo \
   --query "LoadBalancers[0].LoadBalancerArn" --output text)
 
 TGARN=$(aws elbv2 create-target-group --name hostname-app-tg \
   --protocol HTTP --port 80 --target-type ip --vpc-id $VPCID \
   --query "TargetGroups[0].TargetGroupArn" --output text)
 
-aws elbv2 create-listener --load-balancer-arn $LBARN --protocol HTTP \
-  --port 80 --default-actions Type=forward,TargetGroupArn=$TGARN
+aws elbv2 add-tags --resource-arns $TGARN --tags Key=Environment,Value=Demo
+
+LISTENERARN=$(aws elbv2 create-listener --load-balancer-arn $LBARN --protocol HTTP \
+  --port 80 --default-actions Type=forward,TargetGroupArn=$TGARN \
+  --query "Listeners[0].ListenerArn" --output text)
+
+aws elbv2 add-tags --resource-arns $TGARN --tags Key=Environment,Value=Demo
 
 aws ecr create-repository --repository-name hostname-app \
   --tags Key=Environment,Value=Demo
@@ -119,18 +126,19 @@ $(aws ecr get-login --region $ZONE --no-include-email)
 docker build -t $HOSTNAMEAPPREPOURL:0.1 hostname-app/
 docker push $HOSTNAMEAPPREPOURL:0.1
 
-export HOSTNAMEAPPREPOURL
+export ROLEARN HOSTNAMEAPPREPOURL
 envsubst < hostname-app/task-definition.json.tmpl > task-definition.json
 
 HNTASKREVISION=$(aws ecs register-task-definition --cli-input-json file://task-definition.json \
   --tags key=Environment,value=Demo --query "taskDefinition.revision" --output text)
 
 aws ecs create-service --cluster demo-cluster --service-name hostname-app-service \
-  --task-definition hostname-app:HNTASKREVISION --desired-count 1 --launch-type "FARGATE" \
+  --task-definition hostname-app:HNTASKREVISION --desired-count 2 --launch-type "FARGATE" \
   --scheduling-strategy REPLICA --deployment-controller '{"type": "ECS"}'\
-  --deployment-configuration minimumHealthyPercent=integer,maximumPercent=200
-  --network-configuration "awsvpcConfiguration={subnets=[$SUBNETID]securityGroups=[$SECURITYGROUPID],assignPublicIp=\"ENABLED\"}" \
-  --load-balancers targetGroupArn=$TGARN,containerName=hostname-app,containerPort=8080
+  --deployment-configuration minimumHealthyPercent=100,maximumPercet=200
+  --network-configuration "awsvpcConfiguration={subnets=[$SUBNETID],securityGroups=[$SECURITYGROUPID],assignPublicIp=\"DISABLED\"}" \
+  --load-balancers targetGroupArn=$TGARN,containerName=hostname-app,containerPort=8080 \
+  --tags key=Environment,value=Demo
 
 aws ecs wait services-stable --cluster demo-cluster --services simple-app
 LBURL=$(aws elbv2 describe-load-balancers --query "LoadBalancers[0].DNSName" --output text)
@@ -141,9 +149,10 @@ read
 
 #----------- Cleanup
 
-aws ecs update-service --service hostname-app --cluster demo-cluster --desired-count 0
-aws ecs delete-service --service hostname-app --cluster demo-cluster
-aws ecs wait services-inactive --service hostname-app --cluster demo-cluster
+aws ecs update-service --service hostname-app-service --cluster demo-cluster --desired-count 0
+aws ecs delete-service --service hostname-app-service --cluster demo-cluster
+# This takes some time
+aws ecs wait services-inactive --service hostname-app-service --cluster demo-cluster
 
 aws ecr delete-repository --repository-name hostname-app --force
 aws ecs deregister-task-definition --task-definition hostname-app:$HNTASKREVISION
@@ -151,7 +160,11 @@ aws iam detach-role-policy --role-name ecsTaskExecutionRole --policy-arn $POLICY
 aws iam delete-role --role-name ecsTaskExecutionRole
 aws ecs delete-cluster --cluster demo-cluster
 
+aws elbv2 delete-listener --listener-arn $LISTENERARN
+aws elbv2 delete-target-group --target-group-arn $TGARN
 aws elbv2 delete-load-balancer --load-balancer-arn $LBARN
+# If we don't wait for this, deleting the gateway and vpc fail
+aws elbv2 wait load-balancers-deleted --load-balancer-arn $LBARN
 aws ec2 detach-internet-gateway --internet-gateway-id $GATEWAYID --vpc-id $VPCID
 aws ec2 delete-internet-gateway --internet-gateway-id $GATEWAYID
 aws ec2 delete-subnet --subnet-id $SUBNETID
