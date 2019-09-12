46bc2288655412924a157bfeccf2681c2bcd5571
Ulas Turkmen <ulas@micropsi-industries.com>
2019-09-12 16:10:35 +0200

    hostname app fetch random quote

--- hostname-app/app.py
+++ hostname-app/app.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask, render_template_string
+import requests
 
 app = Flask('hostname-app')
 
-PAGE = """
+INDEX = """
 <html>
   <body>
    <b>Hostname: {{ hostname }} </b>
@@ -11,8 +12,26 @@ PAGE = """
 </html>
 """
 
+RANDOM_QUOTE = """
+<html>
+  <body>
+   <p>{{ quote }}</p>
+   <p>{{ author }}</p>
+  </body>
+</html>
+"""
+
 @app.route("/")
 def index():
-    return render_template_string(PAGE, hostname=os.environ.get('HOSTNAME', 'not available'))
+    return render_template_string(INDEX, hostname=os.environ.get('HOSTNAME', 'not available'))
+
+@app.route("/random-quote/")
+def random_quote():
+    try:
+        result = requests.get("http://localhost:8081/").json()
+    except:
+        result = {"quote": "Service not available", "author": ""}
+    return render_template_string(RANDOM_QUOTE, **result)
+
 
 app.run(host='0.0.0.0', port=8080)
